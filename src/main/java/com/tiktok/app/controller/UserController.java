77bc2188fc106d39f562ee046649464183295ebf
yancy0109 <1156729237@qq.com>
2022-06-20 16:21:07 +0800

    修改为web项目

--- src/main/java/com/tiktok/app/controller/UserController.java
+++ src/main/java/com/tiktok/app/controller/UserController.java
@@ -5,86 +5,91 @@ import com.tiktok.app.response.LoginAndResgiterResponese;
 import com.tiktok.app.response.UserInfo;
 import com.tiktok.app.serivce.UserService;
 import com.tiktok.app.until.JwtUntil;
-import com.tiktok.app.until.ResultResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpSession;
 
 
 /**
- * 用户登陆注册
+ * 用户
  */
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     UserService userService;
 
     @RequestMapping(value = "/douyin/user/register/",method = RequestMethod.POST)
-    public HashMap userRegister(@RequestParam String username, @RequestParam String password){
+    public String userRegister(@RequestParam String username, @RequestParam String password, HttpSession sesson,Model model){
         LoginAndResgiterResponese larResponse = userService.registerUser(username, password);
+
+        //返回注册页
         if (larResponse == null){
-            HashMap map = new HashMap<String,Object>();
-            map.put("status_code",-1);
-            map.put("status_msg","注册失败，用户名重复");
-            map.put("user_id",null);
-            map.put("token",null);
-            return map;
+            model.addAttribute("status_code",-1);
+            model.addAttribute("status_msg","注册失败，用户名重复");
+            sesson.setAttribute("user_id",null);
+            sesson.setAttribute("token",null);
+            return "register";
         }
-        HashMap map = new HashMap<String,Object>();
-        map.put("status_code",0);
-        map.put("status_msg","注册成功");
-        map.put("user_id",larResponse.getUser_id());
-        map.put("token",larResponse.getToken());
-        return map;
+
+        //正常跳转至用户信息
+        model.addAttribute("status_code",0);
+        model.addAttribute("status_msg","注册成功");
+        sesson.setAttribute("user_id",larResponse.getUser_id());
+        sesson.setAttribute("token",larResponse.getToken());
+        return "redirect:/douyin/user/";
     }
 
     @RequestMapping(value = "/douyin/user/login/",method = RequestMethod.POST)
-    public HashMap userLogin(@RequestParam String username,@RequestParam String password){
+    public String userLogin(@RequestParam String username,@RequestParam String password, HttpSession sesson,Model model){
         LoginAndResgiterResponese loginAndResgiterResponese = userService.loginUser(username, password);
+        //返回登录页
         if (loginAndResgiterResponese==null){
-            HashMap map = new HashMap<String,Object>();
-            map.put("status_code",-1);
-            map.put("status_msg","登陆失败,用户名或密码错误");
-            map.put("user_id",null);
-            map.put("token",null);
-            return map;
+            model.addAttribute("status_code",-1);
+            model.addAttribute("status_msg","登陆失败,用户名或密码错误");
+            sesson.setAttribute("user_id",null);
+            sesson.setAttribute("token",null);
+            return "login";
         }
-        HashMap map = new HashMap<String,Object>();
-        map.put("status_code",0);
-        map.put("status_msg","登录成功");
-        map.put("user_id",loginAndResgiterResponese.getUser_id());
-        map.put("token",loginAndResgiterResponese.getToken());
-        return map;
+        //正常跳转至用户信息
+        model.addAttribute("status_code",0);
+        model.addAttribute("status_msg","登录成功");
+        sesson.setAttribute("user_id",loginAndResgiterResponese.getUser_id());
+        sesson.setAttribute("token",loginAndResgiterResponese.getToken());
+        return "redirect:/douyin/user/";
     }
 
     @RequestMapping(value = "/douyin/user/",method = RequestMethod.GET)
-    HashMap userInfo(@RequestParam("user_id")Integer userId,@RequestParam("token")String token){
+    String userInfo(Model model,HttpSession session){
+        Integer userId = (Integer) session.getAttribute("user_id");
+        String token = (String) session.getAttribute("token");
         Integer temp = JwtUntil.parseToken(token);
-        if (temp != userId){
-            HashMap map = new HashMap<String,Object>();
-            map.put("status_code",-1);
-            map.put("status_msg","token无效");
-            map.put("user",null);
+        //返回登录页
+        if (temp.equals(userId)){
+            model.addAttribute("status_code",-1);
+            model.addAttribute("status_msg","token无效");
+            model.addAttribute("user",null);
+            return "login";
         }
         UserInfo userInfo = userService.showUserInfo(userId);
+        //返回登录页
         if (userInfo == null){
-            HashMap map = new HashMap<String,Object>();
-            map.put("status_code",-1);
-            map.put("status_msg","查询失败");
-            map.put("user",null);
+            model.addAttribute("status_code",-1);
+            model.addAttribute("status_msg","查询失败");
+            model.addAttribute("user",null);
+            return "login";
         }
-        HashMap map = new HashMap<String,Object>();
-        map.put("status_code",0);
-        map.put("status_msg","查询成功");
-        map.put("user",userInfo);
-        return map;
+        //正常跳转至用户信息
+        model.addAttribute("status_code",0);
+        model.addAttribute("status_msg","查询成功");
+        model.addAttribute("userinfo",userInfo);
+        return "userinfo";
     }
     @RequestMapping(value = "/",method = RequestMethod.GET)
-    String hello(){
-        return "hello";
+    String index(){
+        return "index";
     }
 }
