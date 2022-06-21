0ed612dca335b2f31dfe063ebb675cbcaea2dfcd
bai-ger <104897658+bai-ger@users.noreply.github.com>
2022-06-21 11:45:06 +0800

    添加评论

--- src/main/java/com/tiktok/app/config/WebConfig.java
+++ src/main/java/com/tiktok/app/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")     //所有请求都被拦截 包括静态资源
-                .excludePathPatterns("/","/register","/index","/login","/css/**","/js/**","/cover/**","/video/**","/douyin/user/login/","/douyin/user/register/","/douyin/comment/action/","/favicon.ico");     //放行的请求;
+                .excludePathPatterns("/","/register","/index","/login","/css/**","/js/**","/cover/**","/video/**","/douyin/user/login/","/douyin/user/register/","/favicon.ico");     //放行的请求;
         WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
