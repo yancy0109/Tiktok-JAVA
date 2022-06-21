e0f39588f509863cbb9cabb8bf8272acd545c360
bai-ger <104897658+bai-ger@users.noreply.github.com>
2022-06-21 16:37:14 +0800

    添加评论

--- src/main/java/com/tiktok/app/mapper/CommentMapper.java
+++ src/main/java/com/tiktok/app/mapper/CommentMapper.java
@@ -11,7 +11,7 @@ public interface CommentMapper {
     @Insert("insert into comment(video_id, author_id, content, create_date,  status) values (#{videoid},#{authorid},#{content},#{createdate},#{status})")
     int insertComment(Comment comment);
 
-    @Select("select author_id from comment where comment_id= #{commentId}")
+    @Select("select author_id from comment where id= #{commentId}")
     int InquaryAuthor(Integer commentId);
 
     @Update("update comment set status= #{action_type}")
