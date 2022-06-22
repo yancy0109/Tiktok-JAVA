99dbcfbabd24e65bc38fd42f288061e1461b8ef8
yancy0109 <1156729237@qq.com>
2022-06-22 09:48:40 +0800

    评论列表查看

--- src/main/java/com/tiktok/app/mapper/CommentMapper.java
+++ src/main/java/com/tiktok/app/mapper/CommentMapper.java
@@ -6,12 +6,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import java.util.ArrayList;
+
 @Mapper
 public interface CommentMapper {
     @Insert("insert into comment(video_id, author_id, content, create_date,  status) values (#{videoid},#{authorid},#{content},#{createdate},#{status})")
     int insertComment(Comment comment);
 
-    @Select("select author_id from comment where comment_id= #{commentId}")
+    @Select("select author_id from comment where id = #{commentId}")
     int InquaryAuthor(Integer commentId);
 
     @Update("update comment set status= #{action_type}")
@@ -20,4 +22,11 @@ public interface CommentMapper {
 
     @Select("select count(*) from comment where video_id = #{videoId} and status = 1")
     Integer countComment(Integer videoId);
+
+    @Select("select comment.id from comment where video_id = #{videoId}")
+    ArrayList<Integer> getCommentList(Integer videoId);
+
+
+    @Select("select video_id,author_id,content,create_date,status from comment where id = #{commentId} and status = 1")
+    Comment getCommentById(Integer commentId);
 }
--- src/main/java/com/tiktok/app/serivce/lmpl/CommentServiceImpl.java
+++ src/main/java/com/tiktok/app/serivce/lmpl/CommentServiceImpl.java
@@ -2,18 +2,36 @@ package com.tiktok.app.serivce.lmpl;
 
 
 import com.tiktok.app.bean.Comment;
-import com.tiktok.app.mapper.CommentMapper;
+import com.tiktok.app.bean.User;
+import com.tiktok.app.bean.Video;
+import com.tiktok.app.mapper.*;
+import com.tiktok.app.response.CommentInfo;
+import com.tiktok.app.response.CommentList;
+import com.tiktok.app.response.UserInfo;
+import com.tiktok.app.response.VideoInfo;
 import com.tiktok.app.serivce.CommentService;
+import com.tiktok.app.serivce.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import java.util.ArrayList;
 import java.util.Date;
 
 @Service
 public class CommentServiceImpl implements CommentService {
     @Autowired
     CommentMapper commentMapper;
+    @Autowired
+    VideoMapper videoMapper;
+    @Autowired
+    UserMapper userMapper;
+    @Autowired
+    VideoFavoriteMapper videoFavoriteMapper;
+    @Autowired
+    FollowMapper followMapper;
+    @Autowired
+    UserService userService;
 
     @Override
     public boolean saveComment(Integer authorId, Integer videoId, boolean actionType, String context) {
@@ -35,5 +53,43 @@ public class CommentServiceImpl implements CommentService {
         return true;
     }
 
-
+    @Override
+    public CommentList getCommentList(Integer videoId,Integer userId, boolean hasUserId) {
+        CommentList commentList = new CommentList();
+        Video video = videoMapper.selectVideoById(videoId);
+        if(video == null){
+            return null;
+        }
+        int favoriteCount = videoFavoriteMapper.selectVideoFavorite(videoId);
+        Integer commentCount = commentMapper.countComment(videoId);
+        User author = userMapper.findUserById(video.getAuthorid());
+        if(author == null){
+            return null;
+        }
+        int follow = followMapper.countFollowCountById(author.getUserid());
+        int follower = followMapper.countFollowerCountById(author.getUserid());
+        boolean isFollow;
+        VideoInfo videoInfo;
+        if (hasUserId){
+            int i = followMapper.selectFollowStatus(userId, author.getUserid());
+            isFollow = (i==1);
+             videoInfo = new VideoInfo(videoId, new UserInfo(author.getUserid(), author.getUsername(), follow, follower, isFollow),
+                    video.getPlayurl(), video.getCoverurl(),
+                    favoriteCount, commentCount, video.getTitle());
+        }else {
+            videoInfo = new VideoInfo(videoId,new UserInfo(author.getUserid(),author.getUsername(),follow,follower,false),
+                    video.getPlayurl(),video.getCoverurl(),
+                    favoriteCount,commentCount,video.getTitle());
+        }
+        commentList.setVideoInfo(videoInfo);
+        commentList.setCommentInfo(new ArrayList<>());
+        ArrayList<Integer> commentIdList = commentMapper.getCommentList(videoId);
+        for (Integer commentId : commentIdList) {
+            Comment comment = commentMapper.getCommentById(commentId);
+            commentList.getCommentInfo().add(new CommentInfo(commentId,
+                    userService.showUserInfo(comment.getAuthorid()),
+                    comment.getContent(),comment.getCreatedate()));
+        }
+        return commentList;
+    }
 }
