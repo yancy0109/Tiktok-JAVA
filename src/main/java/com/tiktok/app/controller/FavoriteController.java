282b3c5c2f8bf23a562c114dff829cb92de2fa50
yancy0109 <1156729237@qq.com>
2022-06-23 17:47:46 +0800

    关注与点赞返回修改

--- src/main/java/com/tiktok/app/controller/FavoriteController.java
+++ src/main/java/com/tiktok/app/controller/FavoriteController.java
@@ -2,10 +2,10 @@ package com.tiktok.app.controller;
 
 import com.tiktok.app.serivce.FavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpSession;
  * @author bai
  * @version 1.0
  */
-@Controller
+@RestController
 public class FavoriteController {
 
     @Autowired
@@ -26,18 +26,22 @@ public class FavoriteController {
         int authorId = (Integer) session.getAttribute("user_id");
         if (status==1) {
             if (!favoriteService.favoriteAdd(videoId,authorId,status)) {
-                attributes.addAttribute("upload_msg", "点赞失败");
-                return "redirect:/douyin/favourite/list";
+//                attributes.addAttribute("upload_msg", "点赞失败");
+//                return "redirect:/douyin/favourite/list";
+                return "点赞失败";
             }
-            attributes.addAttribute("upload_msg", "点赞成功");
-            return "redirect:/douyin/favourite/list";
+//            attributes.addAttribute("upload_msg", "点赞成功");
+//            return "redirect:/douyin/favourite/list";
+                return "点赞成功";
         } else {
             if (!favoriteService.favoriteUnAdd(videoId,authorId,status)) {
-                attributes.addAttribute("upload_msg", "取消点赞失败");
-                return "redirect:/douyin/favourite/list";
+//                attributes.addAttribute("upload_msg", "取消点赞失败");
+//                return "redirect:/douyin/favourite/list";
+                return "取消点赞失败";
             }
-            attributes.addAttribute("upload_msg", "取消点赞成功");
-            return "redirect:/douyin/favourite/list";
+//            attributes.addAttribute("upload_msg", "取消点赞成功");
+//            return "redirect:/douyin/favourite/list";
+            return "取消点赞成功";
         }
     }
 
